cf3016d43d49cdc2fee28bf4f3295ceeb19c600c
R <r@home.com>
2020-07-08 16:01:01 -0700

    Adding patch endpoint for customer.

--- src/main/java/guru/springfamework/services/CustomerService.java
+++ src/main/java/guru/springfamework/services/CustomerService.java
@@ -14,4 +14,5 @@ public interface CustomerService {
     CustomerDTO getCustomerByLastName(String lastName);
     CustomerDTO createNewCustomer(CustomerDTO customerDTO);
     CustomerDTO saveCustomerByDTO(Long id, CustomerDTO customerDTO);
+    CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO);
 }
